22a49097f73bfeb492d7402d279b5b84bcc7bbc4
Amit006 <amitnk19@gmail.com>
2022-05-01 16:41:09 +0530

    New Style added and  toster msg added

--- src/app.js
+++ src/app.js
@@ -1,24 +1,32 @@
-import "./styles.css";
+import "./assest/style.css";
 import { useState, useRef } from "react";
 
 export default function App() {
   var interVal = useRef(null);
 
   const [timer, setTimer] = useState(0);
+  const [started, setActive] = useState(false);
+  const [stopTime, setStopTimer] = useState(false);
+  const [reset, setrResetTimer] = useState(false);
 
   const startTimer = (e) => {
     interVal.current = setInterval(() => {
       setTimer((time) => time + 1);
     }, 1000);
+    setrResetTimer(false);
+    setActive(true);
   };
 
   const stopTimer = (e) => {
     clearInterval(interVal.current);
+    setActive(false);
   };
 
   const resetTimer = (e) => {
     clearInterval(interVal.current);
     setTimer(0);
+    setActive(false);
+    setStopTimer(false);
   };
 
   const formatTime = (timerVal) => {
@@ -34,9 +42,12 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <span> {formatTime(timer)} </span>
-      <button onClick={startTimer}> start </button>
-      <button onClick={stopTimer}> stop </button>
-      <button onClick={resetTimer}> reset </button>
+      <span> { started && "Timer Started " } </span>
+      <span> { stopTime && "Timer Stoped " } </span>
+      <span> { reset && "just rest Stoped " } </span>
+      <button onClick={startTimer} disabled={started}> start </button>
+      <button onClick={stopTimer} disabled={!started}> stop </button>
+      <button onClick={resetTimer} disabled={reset}> reset </button>
     </div>
   );
 }
